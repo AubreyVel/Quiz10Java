6de98ad62d52ab5a94ad828087e29ee9c9929c13
AubreyVel <agvelasco@amarillocollege.com>
2025-03-26 13:00:53 -0500

    done

--- Main.java
+++ Main.java
@@ -3,35 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-      
+      //using scanner to record inputs
       Scanner myObj = new Scanner(System.in);
       
-      double moneyOff = 0;
+      double discount = 0;
       double tax = 0;
       double moneyLeft =0;
       double total = 0;
 
-
-      System.out.print("Enter Item Price:");
-
+      //asking for Total price pre tax and discount
+      System.out.print("Enter Total Item Price:");
+      
       double price = myObj.nextDouble();
-
+      //asking user to input sales tax in decimal form
       System.out.print("Enter Sales Tax (In Decimal Form):");
 
       double salesTax = myObj.nextDouble();
-
+      //asking user to input promotional discount in decimal form
       System.out.print("Enter Promotion Discount (In Decimal Form):");
 
       double promotion = myObj.nextDouble();
       
-      //calc how much money is taken off due to promo
-      moneyOff = price * promotion;
+      //calculate how much money is taken off due to promo
+      discount = price * promotion;
       //System.out.println(moneyOff); //testing if output is right
-      //calc how much of the total is left when discount is applied
-      moneyLeft = price - moneyOff;
+      //calculate how much of the total is left when discount is applied
+      moneyLeft = price - discount;
       //System.out.println(moneyLeft);//testing if output is right
       //adding the tax and discounted price for total
-      //calc tax with new total
+      //calculate tax with new total after discount
       tax = moneyLeft * salesTax;
       total = moneyLeft + tax;
       System.out.print("Your total is: " + total);
